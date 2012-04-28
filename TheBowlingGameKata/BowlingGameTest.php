e5d6d918451721221b610ef0ddc723bcd21a4032
Nicolas Schwartz <schwartn@gmail.com>
2012-04-28 17:21:10 +0200

    third test

--- TheBowlingGameKata/BowlingGameTest.php
+++ TheBowlingGameKata/BowlingGameTest.php
@@ -31,5 +31,12 @@ class BowlingGameTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals(20, $this->game->score());
     }
 
+    public function testOneSpare(){
 
+        $this->game->roll(5);
+        $this->game->roll(5);
+        $this->game->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertEquals(16, $this->game->score());
+    }
 }
