6a172119f52234162c60d12d732695037194bcbd
Nicolas Schwartz <schwartn@gmail.com>
2012-04-28 17:16:10 +0200

    second test - part 2

--- TheBowlingGameKata/BowlingGameTest.php
+++ TheBowlingGameKata/BowlingGameTest.php
@@ -6,22 +6,30 @@ include_once('./Game.php');
 
 class BowlingGameTest extends PHPUnit_Framework_TestCase{
 
+    private $game;
+
+    public function setUp(){
+        $this->game = new Game();
+    }
+
+    private function rollMany($n, $pins){
+
+        for($i=0;$i<$n;$i++){
+            $this->game->roll($pins);
+        }
+    }
+
     public function testGutterGame(){
 
-        $game = new Game();
-        for($i=0;$i<20;$i++){
-            $game->roll(0);
-        }
-        $this->assertEquals(0, $game->score());
+        $this->rollMany(20, 0);
+        $this->assertEquals(0, $this->game->score());
     }
 
     public function testAllOnes(){
 
-        $game = new Game();
-        for($i=0;$i<20;$i++){
-            $game->roll(1);
-        }
-        $this->assertEquals(20, $game->score());
+        $this->rollMany(20, 1);
+        $this->assertEquals(20, $this->game->score());
     }
+
 
 }
--- TheBowlingGameKata/Game.php
+++ TheBowlingGameKata/Game.php
@@ -5,13 +5,16 @@
 
 class Game{
 
+    private $score = 0;
+
     public function roll($pins){
 
+        $this->score += $pins;
     }
 
     public function score(){
 
-        return 0;
+        return $this->score;
 
     }
 }
